de18d3b0aa26da7a111ce0d292835c94797aab02
Hasibul Haque <hasibul2363@gmail.com>
2017-07-14 20:02:14 +0600

    added middleware

---
+++ Consumer/Middleware/Middleware.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Common;
+using GreenPipes;
+using MassTransit;
+using SimpleInjector.Extensions.ExecutionContextScoping;
+
+namespace Consumer.Middleware
+{
+
+    public class TestFilter<T> : IFilter<T> where T : class, PipeContext
+    {
+        public void Probe(ProbeContext context)
+        {
+           
+        }
+
+        public async Task Send(T context, IPipe<T> next)
+        {
+            //I want to set value from middleware like following (and then want to get this value from consumer/PerformPaymentCommandHandler)
+            Program.Container.GetInstance<RequestInfo>().Token = "Value from Middleware";
+            //But above line is throwing " Fault: The RequestInfo is registered as 'Execution Context Scope' lifestyle, but the instance is requested outside the context of a Execution Context Scope."
+
+
+            //Then tried with following but no luck
+            /*
+            using (Program.Container.BeginExecutionContextScope())
+            {
+                Program.Container.GetInstance<RequestInfo>().Token = "Value from Middleware";
+                await next.Send(context);
+            }*/
+        }
+    }
+
+    public class TestSpecification<T> : IPipeSpecification<T> where T : class, PipeContext
+    {
+        public IEnumerable<ValidationResult> Validate()
+        {
+            return Enumerable.Empty<ValidationResult>();
+        }
+
+        public void Apply(IPipeBuilder<T> builder)
+        {
+            builder.AddFilter(new TestFilter<T>());
+            
+        }
+    }
+
+    public static class ExampleMiddlewareConfiguratorExtensions
+    {
+        public static void UseTestMiddleware<T>(this IPipeConfigurator<T> configurator) where T : class, PipeContext
+        {
+            configurator.AddPipeSpecification(new TestSpecification<T>());
+        }
+    }
+}
--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -4,21 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Common;
+using Consumer.Middleware;
 using MassTransit;
 using MassTransit.SimpleInjectorIntegration;
 using SimpleInjector;
+using SimpleInjector.Extensions.ExecutionContextScoping;
 
 namespace Consumer
 {
     class Program
     {
+        public static Container Container;
         static void Main(string[] args)
         {
 
             var container = new Container();
-            container.Register<RequestInfo>();
+            container.Options.DefaultScopedLifestyle = new ExecutionContextScopeLifestyle();
+            container.Register<RequestInfo>(Lifestyle.Scoped);
             container.Register<PerformPaymentCommandHandler>();
-
+            container.Verify();
+            Container = container;
 
             var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
             {
@@ -32,6 +37,8 @@ namespace Consumer
                 {
                     e.Consumer<PerformPaymentCommandHandler>(container);
                 });
+
+                cfg.UseTestMiddleware();
             });
            
             busControl.Start();
